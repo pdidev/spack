c817acb58dd959e6afc65289b429aafde679a754
Maitre Maroilles <maroilles@mailo.com>
2020-03-27 17:15:15 +0100

    Compilation done from the subdirectory "paraconf"

--- packages/paraconf/package.py
+++ packages/paraconf/package.py
@@ -10,9 +10,7 @@ class Paraconf(CMakePackage):
        access a Yaml tree on top of libyaml."""
 
     homepage = "https://gitlab.maisondelasimulation.fr/jbigot/libparaconf"
-    url      = 'https://gitlab.maisondelasimulation.fr/jbigot/libparaconf/-/archive/master/libparaconf-master.tar.gz'
     git      = 'https://gitlab.maisondelasimulation.fr/jbigot/libparaconf.git'
-
 
     version('develop', branch='master')
     version('0.4.6',   tag='0.4.6')
@@ -21,16 +19,16 @@ class Paraconf(CMakePackage):
     version('0.4.3',   tag='0.4.3')
     version('0.4.2',   tag='0.4.2')
     version('0.4.1',   tag='0.4.1')
-    version('0.4.0',   tag='0.4.0')
 
     variant('shared',  default=True,   description = 'Build shared libraries rather than static ones')
     variant('profile', default='User', values=('User', 'Devel'), description = 'Profile to use for PDI distribution build', multi=False)
     variant('fortran', default=False,   description = 'Enable Fortran support')
     variant('tests',   default=False,  description = 'Build tests')
 
-    depends_on('cmake@3.5:',     type='build')
-    depends_on('libyaml@:0.2.2', type='build')
+    depends_on('cmake', type='build')
+    depends_on('libyaml', type=('build', 'link', 'run'))
 
+    root_cmakelists_dir = 'paraconf'
     def cmake_args(self):
         args = [
             '-DBUILD_SHARED_LIBS:BOOL={:s}'.format('ON' if '+shared' in self.spec else 'OFF'),
