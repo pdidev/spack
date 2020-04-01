d7795b2d11dc5b2657288ad9bf460a0f6697d3b3
maitre-maroilles <maroilles@mailo.com>
2020-04-01 23:53:46 +0200

    Git support when building BPP with spack

--- packages/bpp/package.py
+++ packages/bpp/package.py
@@ -1,24 +1,7 @@
-# Copyright 2013-2019 Lawrence Livermore National Security, LLC and other
+# Copyright 2013-2020 Lawrence Livermore National Security, LLC and other
 # Spack Project Developers. See the top-level COPYRIGHT file for details.
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
-
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install bpp
-#
-# You can edit this file again by typing:
-#
-#     spack edit bpp
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
 
 from spack import *
 
@@ -30,7 +13,6 @@ class Bpp(CMakePackage):
        compiler."""
 
     homepage = "https://gitlab.maisondelasimulation.fr/jbigot/bpp"
-    url      = 'https://gitlab.maisondelasimulation.fr/jbigot/bpp/-/archive/master/bpp-master.tar.gz'
     git      = 'https://gitlab.maisondelasimulation.fr/jbigot/bpp.git'
 
 
@@ -38,4 +20,5 @@ class Bpp(CMakePackage):
     version('0.3.0',   tag='v0.3.0')
 
     depends_on('cmake@2.8:', type='build')
+    depends_on('git',        type='build')
 
