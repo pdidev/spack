658134d32ae837013649554c666585cce6868b96
maitre-maroilles <maroilles@mailo.com>
2020-04-02 16:06:27 +0200

    Bug fixed for the "indent" option

--- packages/pdi/package.py
+++ packages/pdi/package.py
@@ -37,7 +37,7 @@ class Pdi(CMakePackage):
     depends_on('git',                type='build')
     depends_on('pdi.paraconf')
     depends_on('pdi.bpp')
-    depends_on('astyle@3.1.0:',      type='build', when='+indent')
+    depends_on('astyle@3.1:',        type='build', when='+indent')
     depends_on('doxygen@1.8.13:',    type='build', when='+docs')
     depends_on('py-pybind11@2.3.0:', type='build', when='+python')
     depends_on('python',             when='+python')
