af84851b0108aa3a1bf549f544995a3e3605acaf
Thomas Padioleau <thomas.padioleau@cea.fr>
2021-12-02 16:07:07 +0100

    Add DDC and mdspan

---
+++ packages/DDC/package.py
@@ -0,0 +1,26 @@
+# Copyright (C) 2020 Commissariat a l'energie atomique et aux energies alternatives (CEA)
+# and others. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+
+
+class DDC(CMakePackage):
+    """DDC is a domain decomposition library."""
+
+    homepage = "https://github.com/Maison-de-la-Simulation/ddc"
+    git = "https://github.com/Maison-de-la-Simulation/ddc.git"
+    url = ""
+
+    maintainers = ['tpadioleau', 'jbigot']
+
+    test_requires_compiler = True
+
+    version('main', branch='main', submodules=True)
+
+    depends_on("cmake@3.15:", type='build')
+    depends_on("mdspan")
+    depends_on("pdi")
+    depends_on("googletest", type='test')
+    depends_on("benchmark", type='test')
