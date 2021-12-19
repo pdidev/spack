7e72acd114b798b6074752a3f6eb133ee7575736
Julien Bigot <julien.bigot@cea.fr>
2021-12-19 18:22:12 +0100

    New paraconf versions

--- packages/paraconf/package.py
+++ packages/paraconf/package.py
@@ -15,8 +15,12 @@ class Paraconf(CMakePackage):
 
     maintainers = ['jbigot']
 
-    version(
-        '0.4.9', sha256='e99a01584e07e4d09b026fcd9a39500fbdbc3074a2598a4bc89f400825094c5a')
+    version('0.4.14', sha256='8a07bdf972ce137932b0d5e08036cf90db23b69f7eaabf52eb7d495d1da01d99')
+    version('0.4.13', sha256='28da96ba45bcb826a387488f283baa0c88bc0b00fa74f4c110d444c0b9a8030c')
+    version('0.4.12', sha256='bbbaf462ed23e9a64a4d521ee469ab723fcd86a6dda9a9d9b4dddfd1a58eef93')
+    version('0.4.11', sha256='35f4ba41eaf675ff16ad4f0722a9e2050ee63b073c7e3e67eb74439978599499')
+    version('0.4.10', sha256='0a0028354b131436e70af06c9e029f738ed771088e53633b2b5d1c8ee1276e83')
+    version('0.4.9', sha256='e99a01584e07e4d09b026fcd9a39500fbdbc3074a2598a4bc89f400825094c5a')
 
     variant('shared',  default=True,
             description='Build shared libraries rather than static ones')
@@ -31,11 +35,8 @@ class Paraconf(CMakePackage):
 
     def cmake_args(self):
         args = [
-            '-DBUILD_SHARED_LIBS:BOOL={:s}'.format(
-                'ON' if '+shared' in self.spec else 'OFF'),
-            '-DBUILD_TESTING:BOOL={:s}'.format(
-                'ON' if '+tests' in self.spec else 'OFF'),
-            '-DBUILD_FORTRAN:BOOL={:s}'.format(
-                'ON' if '+fortran' in self.spec else 'OFF')
+            self.define_from_variant('BUILD_SHARED_LIBS', 'shared'),
+            self.define_from_variant('BUILD_TESTING', 'tests'),
+            self.define_from_variant('BUILD_FORTRAN', 'fortran'),
         ]
         return args
