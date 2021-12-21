c13cc850af8886474a9b7a256419558b89fb4a41
Thomas Padioleau <thomas.padioleau@cea.fr>
2021-12-21 11:30:42 +0100

    Add a voicexx recipe

---
+++ packages/voicexx/package.py
@@ -0,0 +1,26 @@
+from spack import *
+
+
+class Voicexx(CMakePackage):
+    """"""
+
+    homepage = "https://gitlab.maisondelasimulation.fr/gysela-developpers/voicexx"
+    git = "https://gitlab.maisondelasimulation.fr/gysela-developpers/voicexx.git"
+    url = ""
+
+    maintainers = ['gdgirard', 'tpadioleau', 'jbigot']
+
+    test_requires_compiler = True
+
+    version('main',  branch='main', submodules=True)
+
+    variant('benchmarks', default=False, description='Build the benchmarks.')
+    variant('deprecated', default=False, description='Enable deprecated code.')
+
+    depends_on("lapack")
+    depends_on("fftw precision=double -mpi")
+    depends_on("paraconf@0.4.14:")
+    depends_on("pdi")
+    depends_on("cmake@3.15:", type='build')
+    depends_on('googletest +gmock', type='test')
+    depends_on("benchmark", type='test', when='+benchmarks')
