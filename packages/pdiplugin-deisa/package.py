2e5dfc407f8387b0449da4d3a6d9dfff426634a0
Thomas Padioleau <thomas.padioleau@cea.fr>
2022-10-14 18:07:21 +0200

    Update deisa related recipes

--- packages/pdiplugin-deisa/package.py
+++ packages/pdiplugin-deisa/package.py
@@ -5,18 +5,17 @@ class PdipluginDeisa(CMakePackage):
     """The Deisa plugin let one use Deisa through PDI."""
 
     git = 'https://github.com/GueroudjiAmal/deisa.git'
+    url = ''
 
     version('develop', branch='master')
 
     depends_on('cmake@3.10:', type=('build'))
 
-    depends_on('pdi+python@1.4.3', type=('link', 'run'))
-    depends_on('mpi', type=('build', 'link', 'run'))
-    depends_on('pdiplugin-mpi', type=('build', 'link', 'run'))
-    depends_on('py-distributed@deisa', type=('run'))
-    depends_on('py-dask@2021.11.2:', type=('run'))
-    depends_on('py-dask-ml@deisa', type=('run'))
-    depends_on('py-xarray', type=('run'))
+    depends_on('pdi@1.5: +python',     type=('link', 'run'))
+    depends_on('mpi',                  type=('build','link','run'))
+    depends_on('py-deisa@2021.11.2:',  type=('run'))
+
+    root_cmakelists_dir = 'pdiplugin-deisa'
 
     def cmake_args(self):
         args = [
---
+++ packages/py-deisa/package.py
@@ -0,0 +1,24 @@
+# Copyright 2013-2021 Lawrence Livermore National Security, LLC and other Spack Project Developers,
+# Copyright (C) 2021-2022 Commissariat a l'energie atomique et aux energies alternatives (CEA)
+# See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+
+
+class PyDeisa(PythonPackage):
+    """
+    """
+
+    pypi = 'deisa/deisa-0.2.1.tar.gz'
+
+    maintainers = ['GueroudjiAmal']
+
+    version('0.2.1', sha256='c91b8cfcf890f26e24d643ed3eaeae7a5e46bb5db92bd3295fd7d1b7e39bf97b')
+
+
+    depends_on('python@3.9:', type=('build', 'run'))
+    depends_on('py-dask +distributed ^py-distributed@deisa', type=('run'))
+    depends_on('py-setuptools@59:', type='build')
+    depends_on('py-pyyaml', type=('build', 'run'))
