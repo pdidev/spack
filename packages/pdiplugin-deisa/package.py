f37c8de5f40be7d66be813264a504810a3339945
Julien Bigot <julien.bigot@cea.fr>
2022-10-19 11:31:05 +0900

    Update py-deisa to 0.2.5

--- packages/pdiplugin-deisa/package.py
+++ packages/pdiplugin-deisa/package.py
@@ -5,23 +5,19 @@ class PdipluginDeisa(CMakePackage):
     """The Deisa plugin let one use Deisa through PDI."""
 
     git = 'https://github.com/GueroudjiAmal/deisa.git'
-    url = ''
 
     version('develop', branch='master')
 
     depends_on('cmake@3.10:', type=('build'))
 
-    depends_on('pdi@1.5: +python',     type=('link', 'run'))
-    depends_on('mpi',                  type=('build','link','run'))
-    depends_on('py-deisa@2021.11.2:',  type=('run'))
+    depends_on('pdi@1.5: +python', type=('link', 'run'))
+    depends_on('mpi',              type=('build', 'link', 'run'))
+    depends_on('py-deisa@0.2.0:',  type=('run'))
 
-    root_cmakelists_dir = 'pdiplugin-deisa'
+    root_cmakelists_dir = 'pdi-plugin'
 
     def cmake_args(self):
-        args = [
-            '-DINSTALL_PDIPLUGINDIR:PATH={:s}'.format(self.prefix.lib),
-        ]
-        return args
+        return [self.define('INSTALL_PDIPLUGINDIR', self.prefix.lib)]
 
     def setup_dependent_environment(self, spack_env, run_env, dependent_spec):
         run_env.append_path('PDI_PLUGIN_PATH', self.prefix.lib)
--- packages/py-deisa/package.py
+++ packages/py-deisa/package.py
@@ -11,11 +11,11 @@ class PyDeisa(PythonPackage):
     """
     """
 
-    pypi = 'deisa/deisa-0.2.4.tar.gz'
+    pypi = 'deisa/deisa-0.2.5.tar.gz'
 
     maintainers = ['GueroudjiAmal']
 
-    version('0.2.4', sha256='e157bf3ebf052cbdcf9f43f42ba7a99a627de6599d6d152dc49945d0e3da4428')
+    version('0.2.5', sha256='3b5f753b9c6af2f3d76b2d92e268174019aad12fb216e055316ea84fa83605d5')
 
 
     depends_on('python@3.9:', type=('build', 'run'))
